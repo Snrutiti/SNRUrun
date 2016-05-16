a2fd6f3f89428f3ac2a17fb013468e8efde3e766
titinun <titinun_02@hotmail.com>
2016-05-16 13:35:07 -0700

    Intent to SignUp

--- app/src/main/java/snru/boonkun/titinun/snrurun/MainActivity.java
+++ app/src/main/java/snru/boonkun/titinun/snrurun/MainActivity.java
@@ -1,7 +1,9 @@
 package snru.boonkun.titinun.snrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   //main method นี่คือ เมท็อด
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this,SignUp.class));
+
     }
-}
+
+
+}   //main class นี่คือ คลาสหลัก
+
